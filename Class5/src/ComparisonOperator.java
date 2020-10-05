fe8236abb728b9629fd90b548d9374f6a9ef5466
YoonSangJoon <yundosa2@gmail.com>
2020-10-05 15:51:20 +0900

    관계 연산자 예제

---
+++ Class5/src/ComparisonOperator.java
@@ -0,0 +1,16 @@
+
+public class ComparisonOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 4;
+		
+		System.out.print((x == y) + " "); // false
+		System.out.print((x != y) + " "); // true
+		System.out.print((x > y) + " "); // false
+		System.out.print((x < y) + " "); // true
+		System.out.print((x <= y) + " "); // true
+	}
+
+}
