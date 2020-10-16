32ebd018db3a1a97a17c0e5805fa53e0b2bbc0be
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-10-16 22:50:13 +0900

    배열 만들기 예제 1

---
+++ Class7/src/array/ArrayTest0_1.java
@@ -0,0 +1,14 @@
+package array;
+
+public class ArrayTest0_1 {
+
+	public static void main(String[] args) {
+		int iArr3[] = { 10, 20, 30, 40, 50};
+		
+		for (int i = 0; i < iArr3.length; i++) {
+			System.out.println(iArr3[i]);
+		}
+
+	}
+
+}
