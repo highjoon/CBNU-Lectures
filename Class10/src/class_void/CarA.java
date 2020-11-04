b98a316ab3e3f575de58e881abe13f10a8cea158
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-11-04 15:58:42 +0900

    객체 만들기 : Car 클래스 활용

---
+++ Class10/src/class_void/CarA.java
@@ -0,0 +1,16 @@
+	package class_void;
+
+class CarA {
+	
+	String color;
+	int speed;
+	
+	void upSpeed(int value) {
+		speed += value;
+	}
+	
+	void downSpeed(int value) {
+		speed -= value;
+	}
+}
+
