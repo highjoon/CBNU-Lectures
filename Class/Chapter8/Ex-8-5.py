d7d9e2643b27e26720c45d5917a463df2a95192a
YoonSangJoon <63948484+YoonSangJoon@users.noreply.github.com>
2020-11-13 10:53:32 +0900

    Matplotlib Stem Plot 실습

---
+++ Class/Chapter8/Ex-8-5.py
@@ -0,0 +1,14 @@
+# Stem Plot
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# linspace(start, stop, num) / start : 배열의 시작값, stop : 배열의 끝값, num : start와 stop 사이를 몇 개의 일정한 간격으로 요소를 만들 것인지를 나타내는 값.
+# np.pi : 원주율 (3.14)
+x = np.linspace(0.1, 2 * np.pi, 10)
+plt.title("Stem Plot")
+
+# use_line_collection : 중간에 가로축 길게 그리기
+# -.  :  선 모양
+plt.stem(x, np.cos(x), '-.', use_line_collection=True)
+plt.show()
