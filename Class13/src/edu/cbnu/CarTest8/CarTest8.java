fb054338571ad0a90bc83dad2be3a5a2298794da
YoonSangJoon <yundosa2@gmail.com>
2020-12-04 23:47:51 +0900

    추상 클래스 구현

---
+++ Class13/src/edu/cbnu/CarTest8/CarTest8.java
@@ -0,0 +1,13 @@
+package edu.cbnu.CarTest8;
+
+public class CarTest8 {
+
+	public static void main(String[] args) {
+		
+		Sedan sedan1 = new Sedan();
+		System.out.println("승용차 인스턴스 생성");
+		Truck truck1 = new Truck();
+		System.out.println("트럭 인스턴스 생성");
+	}
+
+}
