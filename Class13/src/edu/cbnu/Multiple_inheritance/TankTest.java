f908c8f70c4b357eb1bf0a55771f82d96f60ae39
YoonSangJoon <yundosa2@gmail.com>
2020-12-05 00:03:26 +0900

    인터페이스 다중상속

---
+++ Class13/src/edu/cbnu/Multiple_inheritance/TankTest.java
@@ -0,0 +1,12 @@
+package edu.cbnu.Multiple_inheritance;
+
+public class TankTest {
+
+	public static void main(String[] args) {
+		Tank tank1 = new Tank();
+		tank1.work();
+		tank1.fire();
+
+	}
+
+}
