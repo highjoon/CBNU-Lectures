bdf0f338e433ecd424c6e1ad0d38ac3e6edb1a0d
YoonSangJoon <yundosa2@gmail.com>
2020-12-04 22:50:29 +0900

    상속의 제한

---
+++ Class12/src/edu/cbnu/CarTest4/Car.java
@@ -0,0 +1,13 @@
+package edu.cbnu.CarTest4;
+
+public class Car {
+	private String color;
+	int speed;
+
+}
+
+class Sedan extends Car {
+	void setSpeed(int speed) {
+		this.speed = speed;
+	}
+}
---
+++ Class12/src/edu/cbnu/CarTest4/CarTest4.java
@@ -0,0 +1,13 @@
+package edu.cbnu.CarTest4;
+
+public class CarTest4 {
+
+	public static void main(String[] args) {
+		
+		Sedan sedan1 = new Sedan();
+		
+		sedan1.setSpeed(300);
+		System.out.println("½Â¿ëÂ÷ ¼Óµµ ==> " + sedan1.speed);
+	}
+
+}
