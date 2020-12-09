88e6295d2869851628fa5d29b796cbd267df9e19
YoonSangJoon <yundosa2@gmail.com>
2020-12-09 20:03:31 +0900

    파일 - 기존 파일을 Overwrite하여 데이터 기록하기

---
+++ Class/Chapter6/File_3.py
@@ -0,0 +1,15 @@
+import os.path
+
+outfile = open("C:\\Pythonsource\\Class\\CBNU_Advanced_Business_Programming\\Class\\Chapter6\\phones.txt", "w")
+
+outfile.write("홍길동1 010-1234-5678\n")
+outfile.write("김철수1 010-1234-5678\n")
+outfile.write("김영희1 010-1234-5680\n")
+
+outfile.close()
+
+"""
+홍길동1 010-1234-5678
+김철수1 010-1234-5678
+김영희1 010-1234-5680
+"""
