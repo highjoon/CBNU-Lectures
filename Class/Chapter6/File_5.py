a7c74f85ed11230d535cd9b45e093b0972119b8d
YoonSangJoon <yundosa2@gmail.com>
2020-12-09 20:28:09 +0900

    파일 - 파일에서 단어 읽기

--- Class/Chapter6/File_5.py
+++ Class/Chapter6/File_5.py
@@ -4,7 +4,7 @@ for line in infile:
     line = line.rstrip()
     # 파이썬 rstrip ()은 문자열의 지정된 문자열의 끝을 (기본값은 공백입니다) 삭제합니다.
     word_list = line.split()
-    # split 함수는 a.split()처럼 괄호 안에 아무 값도 넣어 주지 않으면 공백(스페이스, 탭, 엔터 등)을 기준으로 문자열을 나누어 준다.
+    # split 함수는 a.split()처럼 괄호 안에 아무 값도 넣어 주지 않으면 공백(스페이스, 탭, 엔터 등)을 기준으로 문자열을 나누어주고 리스트로 저장.
     for word in word_list:
         print(word)
 
