5d75df1697c3543064d5bc3a58ded1a816fcd44e
YoonSangJoon <yundosa2@gmail.com>
2020-12-10 00:17:24 +0900

    Matplotlib 막대 그래프 실습 2

--- Class/Chapter8/Ex-8-3-1.py
+++ Class/Chapter8/Ex-8-3-1.py
@@ -5,11 +5,11 @@ import numpy as np # 수치계산 라이브러리
 
 y = [2,3,1]
 
-# 배열 [0,1,2]
+x = np.arange(len(y))   # 배열 [0,1,2]
+
 # numpy.arange([start, ] stop, [step, ] dtype=None)
 # numpy 모듈의 arange 함수는 반열린구간 [start, stop) 에서 step 의 크기만큼 일정하게 떨어져 있는 숫자들을 array 형태로 반환해 주는 함수다.
 
-x = np.arange(len(y))
 xlabel = ['a', 'b', 'c']
 
 plt.title("Bar Chart")
@@ -21,6 +21,7 @@ plt.xticks(x, xlabel)
 # plt.yticks(sorted(y))
 plt.yticks(y)
 
-plt.xlabel("abc")
-plt.ylabel("count")
+
+plt.xlabel("abc")           # 가로 라벨 이름
+plt.ylabel("count")       # 세로 라벨 이름
 plt.show()
