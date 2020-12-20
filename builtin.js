018a30cbcedd759538c960df0d63c257590f24df
YoonSangJoon <yundosa2@gmail.com>
2020-12-21 07:58:23 +0900

    os, path 모듈

--- builtin.js
+++ builtin.js
@@ -1,6 +1,13 @@
 const os = require('os');
+const path = require('path');
 
-console.log(os.arch());
-// console.log(os.cpus());
-console.log(os.freemem());
-console.log(os.totalmem());
+// console.log(os.arch());
+// // console.log(os.cpus());
+// console.log(os.freemem());
+// console.log(os.totalmem());
+
+console.log(path.sep);
+console.log(__filename);
+console.log(path.dirname(__filename));
+console.log(path.extname(__filename));
+console.log(path.basename(__filename, '.js'));
