5219f86e5fc31a67ce3f49b689680aaa2e10cb2a
YoonSangJoon <yundosa2@gmail.com>
2020-12-22 23:07:48 +0900

    make it better

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 const User = require('./user');
 const Good = require('./good');
 const Auction = require('./auction');
@@ -7,7 +7,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 
-const sequelize = new sequelize(
+const sequelize = new Sequelize(
   config.database, config.username, config.password, config,
 );
 
